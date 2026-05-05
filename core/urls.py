67d072b816d8b085b773c796320c31a56b58c96d
andrijlis15-source <andrijlis15@gmail.com>
2026-05-05 19:57:15 +0200

    lab6 product and category filtering completed

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('categories/', views.categories),
+    path('products/', views.products),
+    path('category/<int:pk>/', views.category_products),
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from .models import Category, Product
 
 def home(request):
     return render(request, 'home.html')
+
+def categories(request):
+    categories = Category.objects.all()
+    return render(request, 'categories.html', {'categories': categories})
+
+def products(request):
+    products = Product.objects.all()
+    return render(request, 'products.html', {'products': products})
+
+def category_products(request, pk):
+    products = Product.objects.filter(category_id=pk)
+    return render(request, 'products.html', {'products': products})
