268010b1204db684577820d8622ae9ec5576c240
andrijlis15-source <andrijlis15@gmail.com>
2026-05-05 19:47:59 +0200

    lab5 homepage + base template

--- core/views.py
+++ core/views.py
@@ -1,11 +1,4 @@
 from django.shortcuts import render
-from .models import Product, Category
 
 def home(request):
-    products = Product.objects.all()
-    categories = Category.objects.all()
-
-    return render(request, "home.html", {
-        "products": products,
-        "categories": categories
-    })
+    return render(request, 'home.html')
--- mysite/urls.py
+++ mysite/urls.py
@@ -4,12 +4,9 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('', include('core.urls')),
 
-    path('login/', auth_views.LoginView.as_view(
-        redirect_authenticated_user=True,
-        next_page='/'
-    )),
-
-    path('logout/', auth_views.LogoutView.as_view(next_page='/')),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
