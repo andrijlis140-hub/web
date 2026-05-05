0199327ce3c87f6316f4c1ba69ba7f406f381696
andrijlis15-source <andrijlis15@gmail.com>
2026-05-05 19:39:07 +0200

    lab4 models and admin setup completed

--- core/models.py
+++ core/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
+
 class Category(models.Model):
     name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
@@ -11,6 +13,16 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.IntegerField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
+
+
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.product.name} x {self.quantity}"
